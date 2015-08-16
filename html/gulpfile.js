9596dca474c8fcbe77b154ddebcc36fda91c8d35
kyaido <kamehameha1985@gmail.com>
2015-08-16 23:39:32 +0900

    commit

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -1,23 +1,46 @@
 var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps   = require('gulp-sourcemaps');
+var minifyCss    = require('gulp-minify-css');
 var plumber      = require('gulp-plumber');
 var webserver    = require('gulp-webserver');
+var source       = require('vinyl-source-stream');
+var browserify   = require('browserify');
+var buffer       = require('vinyl-buffer');
+var uglify       = require('gulp-uglify');
 
 var AUTOPREFIXER_BROWSERS = [
-  'last 2 version', 'Explorer >= 9', 'Android >= 4.0'
+  'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
 ];
 
 /* sass task */
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(sass({
       errLogToConsole: true,
-      outputStyle: 'compressed'
+      // outputStyle: 'compressed'
     }))
+    .pipe(minifyCss({advanced: false}))
     .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(sourcemaps.write('maps/'))
     .pipe(gulp.dest('dist/css/'));
+});
+
+
+/* js task */
+gulp.task('js', function() {
+  return browserify({
+    entries: ['src/js/main.js']
+  })
+  .bundle()
+  .pipe(plumber())
+  .pipe(source('main.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .pipe(gulp.dest('dist/js'));
 });
 
 
@@ -26,10 +49,10 @@ gulp.task('webserver', function() {
   gulp.src('./dist/')
     .pipe(webserver({
       host: '0.0.0.0',
-      port: 9000,
+      port: 8000,
       livereload: true,
       directoryListing: false,
-      open: 'http://localhost:9000'
+      open: 'http://localhost:8000'
     }));
 });
 
@@ -37,6 +60,7 @@ gulp.task('webserver', function() {
 /* watch task */
 gulp.task('watch', function() {
   gulp.watch(['src/scss/**/*.scss'], ['sass']);
+  gulp.watch(['src/js/**/*.js'], ['js']);
 });
 
 
