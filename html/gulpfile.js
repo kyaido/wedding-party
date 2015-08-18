dd664bf3120a50723063980014c2af7676b62d2a
kyaido <kamehameha1985@gmail.com>
2015-08-19 00:49:27 +0900

    commit

--- html/gulpfile.js
+++ html/gulpfile.js
@@ -11,7 +11,7 @@ var buffer       = require('vinyl-buffer');
 var uglify       = require('gulp-uglify');
 
 var AUTOPREFIXER_BROWSERS = [
-  'last 2 version', 'Explorer >= 8', 'Android >= 2.3'
+  'last 2 version', 'Explorer >= 9', 'Android >= 2.3'
 ];
 
 /* sass task */
