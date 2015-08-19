286d2f2f7975d9d6784e789deb4a8252375db777
kyaido <kamehameha1985@gmail.com>
2015-08-19 12:06:55 +0900

    commit

--- html/src/js/main.js
+++ html/src/js/main.js
@@ -1,1 +1,11 @@
 var $ = require('jquery');
+// var backstretch = require('jquery-backstretch');
+
+// $('.js-backstretch').backstretch([
+//   'img/pic_header_01.jpg',
+//   'img/pic_header_02.jpg',
+//   'img/pic_header_03.jpg',
+//   'img/pic_header_04.jpg',
+//   'img/pic_header_05.jpg',
+//   'img/pic_header_06.jpg',
+// ], {duration: 1000});
