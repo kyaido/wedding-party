1887ff89210fceb8151452bfce53321175ab3c75
kyaido <kamehameha1985@gmail.com>
2015-08-19 16:46:10 +0900

    commit

--- html/src/js/main.js
+++ html/src/js/main.js
@@ -1,11 +1,14 @@
-var $ = require('jquery');
-// var backstretch = require('jquery-backstretch');
+var $;
+window.jQuery = $ = require('jquery');
+var backstretch = require('jquery-backstretch');
 
-// $('.js-backstretch').backstretch([
-//   'img/pic_header_01.jpg',
-//   'img/pic_header_02.jpg',
-//   'img/pic_header_03.jpg',
-//   'img/pic_header_04.jpg',
-//   'img/pic_header_05.jpg',
-//   'img/pic_header_06.jpg',
-// ], {duration: 1000});
+$(function() {
+  $('.js-backstretch').backstretch([
+    'img/pic_header_01.jpg',
+    'img/pic_header_02.jpg',
+    'img/pic_header_03.jpg',
+    'img/pic_header_04.jpg',
+    'img/pic_header_05.jpg',
+    'img/pic_header_06.jpg',
+  ], {duration: 4000, fade: 1000});
+});
